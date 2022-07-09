1b08a1d809438fc35606c4b5078bbe63e2f4df9c
IRules <irulesmain@gmail.com>
2022-07-09 11:43:03 +0300

    Fixed massive homepage bug.

--- components/HomeCard.js
+++ components/HomeCard.js
@@ -3,42 +3,48 @@ import styles from '../styles/Home.module.css';
 import Image from 'next/image';
 import { Avatar, Tooltip } from '@mui/material';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import { debounce } from 'lodash';
 
 function HomeCard() {
   const [hover, setHover] = React.useState(false);
   const verified = false;
+  const handleHandleEnter = debounce(() => setHover(true), 500);
+  const handleMouseLeave = () => {
+    setHover(false);
+    handleHandleEnter.cancel();
+  };
   return (
-    <a
-      href=""
-      onMouseEnter={(e) => {
-        setHover(true);
-      }}
-      onMouseLeave={(e) => setHover(false)}
-    >
+    <a href="">
       <div className={styles.homeCard}>
         <div className={styles.homeCard__content}>
-          {hover ? (
-            <video
-              src={`/api/streamVideo`}
-              width={290}
-              height={180}
-              autoPlay
-              muted
-              controls
-              controlsList="nodownload"
-              disablePictureInPicture
-              style={{ zIndex: 999999 }}
-            />
-          ) : (
-            <Image
-              style={{ zIndex: 0 }}
-              src="/assets/hq720.jpg"
-              alt="Video"
-              width={290}
-              height={180}
-              className={styles.homeCard__contentImage}
-            />
-          )}
+          <div
+            className={styles.homeCard__contentImage}
+            onMouseEnter={handleHandleEnter}
+            onMouseLeave={handleMouseLeave}
+          >
+            {hover ? (
+              <video
+                src={`/api/streamVideo`}
+                width={290}
+                height={180}
+                autoPlay
+                muted
+                controls
+                controlsList="nodownload"
+                disablePictureInPicture
+                style={{ zIndex: 999999 }}
+              />
+            ) : (
+              <Image
+                style={{ zIndex: 0 }}
+                src="/assets/hq720.jpg"
+                alt="Video"
+                width={290}
+                height={180}
+                className={styles.homeCard__contentImage}
+              />
+            )}
+          </div>
           <div className={styles.homeCard__contentInfo}>
             <Tooltip title="Delete">
               <Avatar variant="rounded" />
